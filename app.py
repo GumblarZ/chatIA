420557f6d87095d140294d33681dd1b6ef8f796a
José Paiva <gumblarz@outlook.com>
2025-02-18 11:52:44 -0300

    criado a conexao com deep seek

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
-from config import porta , caminho
+from openai import OpenAI
+from module.config import porta
 
-print(porta)
-print(caminho)
+client = OpenAI(base_url=f"http://localhost:{porta}/v1" , api_key="lm-studio")
 
+conversations =[{
+    "role": "system",
+    "content": "reponda como se fosse um palmeirense fanatico"
+}]
+
+def generate(text):
+    conversations.append({
+        "role":"user",
+        "content": text    
+    })
+
+complention = client.chat.completions.create(model="model-identifier", messages=conversations, temperature=0.7)
+response = complention.choices[0].message
+conversations.append({
+    "role": response.role,
+    "content": response.content
+})
+
+print(f"Assistente: {response.content}")
+while True:
+    
+    user_input = str(input("Voce: "))
+    response = generate(user_input)
+    print(f"Assistente: {response}")
